31031ff3e54f330e299ad81011168d47bf6b9e1a
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-11-19 12:34:41 +0530

    completed home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,25 +3,25 @@ import { FiMoon } from "react-icons/fi";
 import Profile from "../components/Profile";
 export default function Home() {
   return (
-    <div className="h-screen w-full bg-neutral-900/70">
-      <section className="w-full ">
-        <header className="w-full py-20  ">
-          <nav className="w-full max-w-lg mx-auto flex items-center gap-50 justify-between">
-            <div className="w-1/2 rounded-full overflow-hidden ">
+    <div className="h-screen w-full ">
+      <section className="w-full flex px-20 items-center  justify-between ">
+        <header className="w-[80%]   flex items-center  justify-center py-10   ">
+          <nav className="w-full flex items-center   justify-end">
+            <div className="w-1/2 max-w-lg flex ml-40 justify-end mx-auto rounded-full overflow-hidden ">
               <img
-                className="w-120 scale-[1.5]  object-cover object-center "
+                className="w-80  mx-10 object-cover object-center "
                 src="./logo-removebg-preview.png"
                 alt=""
               />
             </div>
 
-            <div className="w-1/2 h-full flex items-center justify-end right  ">
+            <div className="w-1/2 h-full flex px-10 items-center justify-end right  ">
               <FiMoon size={"25"} className="text-end" />
             </div>
           </nav>
         </header>
       </section>
-      <main className="w-full h-full flex  px-40 ">
+      <main className="w-full flex  px-40 ">
         <Profile />
       </main>
     </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,21 +2,34 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <div>
-      <h1 className="fontSt text-7xl tracking-tight px-90 text-center font-extrabold  ">
+    <div className="about w-full  flex item-end flex-col gap-20 ">
+      <h1 className="fontSt text-7xl  tracking-tight px-80 text-start font-extrabold  ">
         Welcome
       </h1>
-      <p className=" w-1/2 px-40 ">
-        Hi, I'm Yaron ( Ya • rone ) — a Product Designer based in beautiful
-        Connecticut, where I live with my amazing wife, two wonderful daughters,
-        a goofy pup, and a grumpy old cat. I’ve worn many hats in the past 25
-        years — product designer, entrepreneur, mentor, and strategist. I’ve
-        worked for world-class companies, built startups from scratch, led them
-        to profitability or acquisition, and even experienced the thrill of an
-        IPO as an early-stage employee. Outside of work, you’ll find me reading
-        historical non-fiction, making music, cooking up a storm, or traveling
-        to new places. Welcome to my website, feel free to poke around.
-      </p>
+      <div className="para py-10 w-full px-80 flex item-center flex-col justify-center ">
+        <p className="text-lg py-14 font-normal font-mono">
+          Hi, I'm ऋषिः ( ऋषभः • २. ) — a Product Designer based in beautiful
+          Connecticut, where I live with my amazing wife, two wonderful
+          daughters, a goofy pup, and a grumpy old cat.
+        </p>
+        <p className="text-lg py-14 font-normal font-mono">
+          I’ve worn many hats in the past 25 years — product designer,
+          entrepreneur, mentor, and strategist. I’ve worked for world-class
+          companies, built startups from scratch, led them to profitability or
+          acquisition, and even experienced the thrill of an IPO as an
+          early-stage employee.
+        </p>
+
+        <p className="text-lg py-14 font-normal font-mono">
+          Outside of work, you’ll find me reading historical non-fiction, making
+          music, cooking up a storm, or traveling to new places.
+        </p>
+
+        <p className="text-lg py-14 font-normal font-mono">
+          {" "}
+          Welcome to my website, feel free to coorect me....
+        </p>
+      </div>
     </div>
   );
 };
